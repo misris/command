268cfed957920a611a989aa7b493258647917d3d
TS <amisris@choco.local>
2017-12-25 23:34:24 +0900

    initial commit

---
+++ cat_misris/cat_misris.cpp
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 2048
+
+static void do_cat(const char *path);
+static void die(const char *s);
+
+int main(int argc, char *argv[]) {
+  int i;
+
+  // arg
+  if (argc < 2) {
+    fprintf(stderr, "%s: file name not given\n", argv[0]);
+    exit(1);
+  }
+  // do cat command
+  for (i = 1; i < argc; i++) {
+    do_cat(argv[i]);
+  }
+
+  return 0;
+}
+
+static void do_cat(const char *path) {
+  int fd;
+  // stream
+  fd = open(path, O_RDONLY);
+  if (fd < 0)
+    die(path);
+
+  // read & write
+  int n;
+  unsigned char buf[BUFFER_SIZE];
+  for (;;) {
+    // read
+    n = read(fd, buf, sizeof buf);
+    if (n < 0)
+      die(path);
+    if (n == 0)
+      break;
+    // write
+    if (write(STDOUT_FILENO, buf, n) < 0)
+      die(path);
+  }
+
+  if (close(fd) < 0)
+    die(path);
+}
+
+static void die(const char *s) {
+  perror(s);
+  exit(1);
+}
