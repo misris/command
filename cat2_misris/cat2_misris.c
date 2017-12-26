65c04470f60b6dacca37487c0f4b725816465053
misris <takamisris@gmail.com>
2017-12-26 17:54:16 +0900

    add line counter

--- cat2_misris/cat2_misris.c
+++ cat2_misris/cat2_misris.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < argc; i++) {
     FILE *f;
     int c;
+    int new_line_count = 1;
 
     // ここで既にバッファに読み込まれている
     // fはファイルディスクリプタとバッファをもつ
@@ -17,11 +18,15 @@ int main(int argc, char *argv[]) {
 
     // 1バイトずつfのバッファから読み込み
     while ((c = fgetc(f)) != EOF) {
+      if (c == 10) {
+        new_line_count++;
+      }
       // 1バイトずつstdoutのストリームに出力
       if (putchar(c) < 0)
         exit(1);
     }
 
+    printf("\n%d行\n", new_line_count);
     fclose(f);
   }
 
